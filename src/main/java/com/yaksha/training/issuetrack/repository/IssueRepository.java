002599d6679a9a2c8b00fc77b7315bd4dea7522c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-13 17:53:41 +0530

    Delete src directory

--- src/main/java/com/yaksha/training/issuetrack/repository/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yaksha.training.issuetrack.repository;
-
-import com.yaksha.training.issuetrack.entity.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<Issue, Long> {
-}
